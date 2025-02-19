2d1d86e5e3547f9d48c58b633613af38a777199b
Douglas Wynne <douglascristiano112@gmail.com>
2025-02-19 04:45:48 -0300

    retirando comentários

--- estrutura_banco_de_dadoos.py
+++ estrutura_banco_de_dadoos.py
@@ -2,30 +2,23 @@ from flask import Flask #API
 from flask_sqlalchemy import SQLAlchemy #banco de dados
 
 
-# O SQLALchemy transforma uma classe python em uma tabela no banco de dados
-
-
 # Criar uma API flask
 app = Flask(__name__)
 
 # Criar instância SQLALchemy:
 app.config['SECRET_KEY'] = '#!OUiYH6z' # acesso único à sua aplicação, colocar uma senha difícil
-app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///'blog.db')"  ##colocar as 3 barras /// e o nome do banco_de_dados.db
-                                                             # para se conectar a um banco de dados online pesquisar:
-                                                             # connection string oracle
-                                                             # connection string sql server
-                                                             # connection string "nome do banco de dados"
-# instanciar o sqlalchemy:
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:#Projetoapi@db.qjqmjsjidhtqrxjqnnci.supabase.co:5432/postgres'
+
 db = SQLAlchemy(app)
 db:SQLAlchemy # fazer essa tipagem para evitar erros
 
 # Definir a estrutura da tabela Postagem: id_postagem, titulo, autor
 
-class Postagem(db.Model): #instanciando o db.Model o que permite fazer criação de tabelas, usando todas as estruturas herdadas da classe
+class Postagem(db.Model):
     __tablename__ = 'postagem'
     id_postagem = db.Column(db.Integer, primary_key = True)
     titulo = db.Column(db.String)
-    id_autor = db.Column(db.Integer, db.ForeignKey('autor.id_autor')) # chave estrangeira referenciando uma outra tabela junto com o nome da propriedade que se irá se tornar a chave estrangeira('autor.id_autor')
+    id_autor = db.Column(db.Integer, db.ForeignKey('autor.id_autor')) 
 
 
 # Definir a estrutura da tabela Autor: id_autor, nome, email, senha, admin, postagens
@@ -35,8 +28,8 @@ class Autor(db.Model):
     nome = db.Column(db.String)
     email = db.Column(db.String)
     senha = db.Column(db.String)
-    admin = db.Column(db.Boolean) # verdadeiro ou falso
-    postagens = db.relationship('Postagem') # passa o nome da classe que tem o relacionamento
+    admin = db.Column(db.Boolean)
+    postagens = db.relationship('Postagem')
 
 
 
